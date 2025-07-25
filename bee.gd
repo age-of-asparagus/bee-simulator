8be7405cdb4e300bbec8b859e48555bdfb31b34b
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2025-07-24 19:50:52 -0700

    movement changes + bug fixes

--- bee.gd
+++ bee.gd
@@ -1,14 +1,13 @@
 extends CharacterBody3D
 
+@export var boost_acceleration : float
 @export var forward_acceleration : float
-@export var max_speed : float
-@export var min_speed : float
-@export var drag : float
+@export var drag_coefficient : float
 @export var turn_speed : float
 @export var smooth_time : float
 
-#@onready var starting_vertical_rotation = rotation
-#@onready var starting_horizontal_rotation = rotation
+@onready var starting_vertical_rotation = rotation
+@onready var starting_horizontal_rotation = rotation
 
 @onready var vertical_rotation = rotation.x
 @onready var horizontal_rotation = rotation.y
@@ -20,6 +19,15 @@ extends CharacterBody3D
 
 func _physics_process(delta):
 	
+	apply_rotation(delta)
+	
+	apply_boost(delta)
+	
+	apply_air_resistance(delta)
+	
+	move_and_slide()
+
+func apply_rotation(delta : float):
 	target_vertical_rotation += get_input_vertical_rotation() * turn_speed * delta
 	target_horizontal_rotation += get_input_horizontal_rotation() * turn_speed * delta
 	
@@ -27,21 +35,15 @@ func _physics_process(delta):
 	horizontal_rotation = lerp_angle(horizontal_rotation, target_horizontal_rotation, smooth_time * delta)
 	
 	rotation = Vector3(vertical_rotation, horizontal_rotation, 0)
-	
-	
-	if Input.is_action_pressed("Boost"):
-		velocity += -basis.z * forward_acceleration * delta
-		velocity = velocity.limit_length(max_speed)
-	else:
-		var drag_force = drag * delta
-		if velocity.length() > drag_force:
-			velocity -= velocity.normalized() * drag_force
-	
-	if velocity.length() < min_speed:
-		velocity = -basis.z * min_speed
-	
-	move_and_slide()
 
+func apply_boost(delta : float):
+	if Input.is_action_pressed("Boost"):
+		velocity += -basis.z * boost_acceleration * delta
+	else:
+		velocity += -basis.z * forward_acceleration * delta
+
+func apply_air_resistance(delta : float):
+	velocity -= velocity.normalized() * velocity.length_squared() * drag_coefficient * delta
 
 func get_input_vertical_rotation():
 	var input_vertical_rotation = Input.get_axis("Move_down", "Move_up")
